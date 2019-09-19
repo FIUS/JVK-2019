a62f062e74a9642b3082c0d057283a5c05c15451
Tim-Julian Ehret <tjehret@gmx.de>
2019-09-19 14:05:53 +0200

    Add: Tasks for sheet 2

---
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/tasks/Helper.java
@@ -0,0 +1,24 @@
+/*
+ * This source file is part of the FIUS JVK 2019 project.
+ * For more information see github.com/FIUS/JVK-2019
+ *
+ * Copyright (c) 2019 the FIUS JVK 2019 project authors.
+ * 
+ * This software is available under the MIT license.
+ * SPDX-License-Identifier:    MIT
+ */
+package de.unistuttgart.informatik.fius.jvk2019.tasks;
+
+import de.unistuttgart.informatik.fius.jvk2019.provided.entity.Neo;
+
+/**
+ * TODO: Description
+ * @author Tim-Julian Ehret
+ */
+public class Helper {
+    
+    protected final static int getCoinCount(Neo neo) {
+        // TODO load number of coins 
+        return 0;
+    }
+}
---
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/tasks/Task2_2.java
@@ -0,0 +1,78 @@
+/*
+ * This source file is part of the FIUS JVK 2019 project.
+ * For more information see github.com/FIUS/JVK-2019
+ *
+ * Copyright (c) 2019 the FIUS JVK 2019 project authors.
+ * 
+ * This software is available under the MIT license.
+ * SPDX-License-Identifier:    MIT
+ */
+package de.unistuttgart.informatik.fius.jvk2019.tasks;
+
+import de.unistuttgart.informatik.fius.icge.simulation.Direction;
+import de.unistuttgart.informatik.fius.icge.simulation.Position;
+import de.unistuttgart.informatik.fius.icge.simulation.Simulation;
+import de.unistuttgart.informatik.fius.icge.simulation.tasks.Task;
+import de.unistuttgart.informatik.fius.jvk2019.provided.Coin;
+import de.unistuttgart.informatik.fius.jvk2019.provided.entity.Neo;
+import de.unistuttgart.informatik.fius.jvk2019.provided.entity.Wall;
+
+
+/**
+ * An example task
+ * 
+ * @author Tim-Julian Ehret
+ */
+public abstract class Task2_2 implements Task {
+    
+    /**
+     * the simulation
+     */
+    protected Simulation sim;
+    
+    /**
+     * The spinning neo
+     */
+    protected Neo neo;
+    
+    private boolean flag1;
+    
+    @Override
+    public void prepare(Simulation sim) {
+        this.sim = sim;
+        
+        this.neo = new Neo();
+        
+        sim.getPlayfield().addEntity(new Position(0, 0), this.neo);
+        
+    }
+    
+    @Override
+    public final void solve() {
+        this.flag1 = true;
+        /*
+         * check if there is a field without a coin
+         */
+        for (int i = this.neo.getPosition().getX(); i >= this.neo.getPosition().getX() - 4; i--) {
+            if (this.sim.getPlayfield().getEntitiesOfTypeAt(new Position(i, this.neo.getPosition().getY()), Coin.class, true).isEmpty()) {
+                this.flag1 = false;
+            }
+        }
+    }
+    
+    /**
+     * turns Neo to the left
+     */
+    public abstract void turnLeft();
+    
+    /**
+     * turns Neo around
+     */
+    public abstract void turnAround();
+    
+    @Override
+    public boolean verify() {
+        return this.flag1;
+    }
+    
+}
