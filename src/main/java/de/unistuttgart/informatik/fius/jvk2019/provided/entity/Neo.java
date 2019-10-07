1cf675c7f65336bbb8c58f585617e9c2d372b935
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2019-10-07 14:48:28 +0200

    Add inspection decorators to Neo

--- src/main/java/de/unistuttgart/informatik/fius/jvk2019/provided/entity/Neo.java
+++ src/main/java/de/unistuttgart/informatik/fius/jvk2019/provided/entity/Neo.java
@@ -9,6 +9,8 @@
  */
 package de.unistuttgart.informatik.fius.jvk2019.provided.entity;
 
+import de.unistuttgart.informatik.fius.icge.simulation.inspection.InspectionAttribute;
+import de.unistuttgart.informatik.fius.icge.simulation.inspection.InspectionMethod;
 import de.unistuttgart.informatik.fius.jvk2019.Texture;
 import de.unistuttgart.informatik.fius.jvk2019.provided.exceptions.NeoIsBrokeException;
 import de.unistuttgart.informatik.fius.jvk2019.provided.exceptions.NoCoinException;
@@ -32,6 +34,7 @@ public class Neo extends Human {
      * @throws NoCoinException
      *     when there is no coin
      */
+    @InspectionMethod()
     protected void collectCoin() {
         if (!this.canCollectCoin()) throw new NoCoinException();
         this.collect(this.getCurrentlyCollectableEntities(Coin.class, true).get(0));
@@ -43,6 +46,7 @@ public class Neo extends Human {
      * @throws NeoIsBrokeException
      *     when Neo is broken
      */
+    @InspectionMethod()
     protected void dropCoin() {
         if (!this.canDropCoin()) throw new NeoIsBrokeException();
         this.drop(this.getCurrentlyDroppableEntities(Coin.class, true).get(0));
@@ -52,6 +56,7 @@ public class Neo extends Human {
      * 
      * @return whether Neo can drop a coin
      */
+    @InspectionAttribute
     protected boolean canDropCoin() {
         return this.getCurrentlyDroppableEntities(Coin.class, true).size() > 0;
     }
@@ -60,6 +65,7 @@ public class Neo extends Human {
      * 
      * @return whether there is a coin to collect
      */
+    @InspectionAttribute
     protected boolean canCollectCoin() {
         return this.getCurrentlyCollectableEntities(Coin.class, true).size() > 0;
     }
@@ -90,6 +96,7 @@ public class Neo extends Human {
      * 
      * @return true if neo stands on a field with a phone booth
      */
+    @InspectionAttribute
     public Boolean isOnPhoneBooth() {
         if (this.getPlayfield().getEntitiesOfTypeAt(this.getPosition(), PhoneBooth.class, true).size() > 0) {
             return true;
@@ -147,6 +154,7 @@ public class Neo extends Human {
     /**
      * @return the number of coins in neos wallet
      */
+    @InspectionAttribute
     public int getCoinsInWallet() {
         return this.getInventory().get(Coin.class, true).size();
     }
